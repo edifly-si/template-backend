3fdc590d3dceb5874912bc0ba04dbf23adb99369
Harry Krishnawan <hkthea@users.noreply.github.com>
2022-08-25 15:04:12 +0700

    Create base_signer.js

---
+++ src/library/base_signer.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+// import fs from 'fs';
+
+export default (privateKey, publicKey, signerOptions) => {
+    const verifyToken=(aToken)=>{
+        return jwt.verify(aToken, publicKey, signerOptions);
+    }
+
+    const signer=(uData)=>{
+        // console.log(uData);
+        return jwt.sign(uData, privateKey, signerOptions);
+    }
+
+    const decode=(aToken)=>{
+        try {
+            return verifyToken(aToken) && jwt.decode(aToken, {complete:false});       
+        } catch (error) {
+            return false;
+        }
+    }
+
+    const refreshToken=(aToken)=>{
+        const {aud, exp, iat, sub, ...uData}=decode(aToken);
+        return uData && signer(uData);
+    }
+
+    return { verifyToken, signer, decode, refreshToken }
+}
