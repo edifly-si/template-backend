1f74af3d3800a24ac8be0a19dfb4a45a0a13f323
Harry Krishnawan <hkthea@users.noreply.github.com>
2022-08-25 15:00:14 +0700

    Update utils.js add createReportCtrl

--- src/controller/utils.js
+++ src/controller/utils.js
@@ -143,3 +143,43 @@ export const createFile=(file)=>{
     file.mv(dir+'/'+filename);
     return filename;
 }
+
+/**
+ *  
+ * @param {m.Model} schema 
+ * @param {String} type 
+ * @param {Array} columns 
+ * @param {Function} getReport 
+ * @returns {Router}
+ */
+export const createReportCtrl = (schema, type='daily', columns=[], getReport=false)=>{
+    const rtr=Router();
+    const header=columns.map(({title})=>title);
+    const fields=columns.map(({title, ...rest})=>({...rest}));
+    if(type==='daily'){
+        rtr.get('/:first_date/:last_date',(req, res)=>{
+            CtrlHandler(req, res, async(body)=>{
+                const {first_date, last_date} = req.params;
+                if(typeof getReport==='function'){
+                    const data=getReport(schema, req, res, first_date, last_date);
+                    return {data, header, fields};
+                }
+                throw new Error("Report callback function not found!");
+            })
+        })
+    }
+    else {
+        rtr.get('/:month', (req, res)=>{
+            CtrlHandler(req, res, async(body)=>{
+                const {month} = req.params;
+                if(typeof getReport==='function'){
+                    const data=getReport(schema, req, res, month);
+                    return {data, header, fields};
+                }
+                throw new Error("Report callback function not found!");
+            });
+        })
+    }
+
+    return rtr;
+}
