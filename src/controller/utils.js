8f51a1a784c841734eca1f550c772b5bc8ee22d3
Harry Krishnawan <hkthea@users.noreply.github.com>
2022-08-25 15:02:01 +0700

    Update utils.js
    Add Function Create Auth Controller

--- src/controller/utils.js
+++ src/controller/utils.js
@@ -183,3 +183,77 @@ export const createReportCtrl = (schema, type='daily', columns=[], getReport=fal
 
     return rtr;
 }
+
+/**
+ * 
+ * @param {any} model 
+ * @param {any} decoder 
+ * @param {any} refreshToken 
+ * @returns {Router}
+ */
+export const createAuthController=(model, decoder, refreshToken)=>{
+    const { changePassword, createDefaultUser, Login, updateProfile } = model;
+    const rtr=Router();
+
+    rtr.post('/login', (req, res)=>{
+        CtrlHandler(req, res, async(body)=>{
+            const {username, password}=body;
+            try {
+                const [token, udata]=await Login(username, password);
+                createLog(udata._id, `Login Success For User ${username}`, req);
+                return token;
+            } catch (error) {
+                createLog(undefined, `Login Failed For User ${username}`, req);
+                throw error;
+            }        
+        });
+    });
+    
+    rtr.use('/logout', AuthMiddleware(decoder));
+    rtr.use('/refreshToken', AuthMiddleware(decoder));
+    rtr.use('/profile', AuthMiddleware(decoder));
+    rtr.use('/changePassword', AuthMiddleware(decoder));
+    rtr.use('/me', AuthMiddleware(decoder));
+    
+    rtr.get('/logout', (req, res)=>{
+        CtrlHandler(req, res, async(body)=>{
+            const {_id:user_id, username}=res.locals.udata;
+            createLog(user_id, `${username} Logout`, req);
+            return true;
+        });
+    });
+    
+    rtr.get('/refreshToken', (req, res)=>{
+        CtrlHandler(req, res, async(body)=>{
+            return refreshToken(res.locals.token);
+        });
+    });
+    
+    rtr.get('/me', (req, res)=>{
+        CtrlHandler(req, res, async(body)=>{
+            return res.locals.udata;
+        });
+    });
+    
+    
+    rtr.post('/profile', (req, res)=>{
+        CtrlHandler(req, res, async(body)=>{
+            // console.log(body);
+            const {_id, username}=res.locals.udata;
+            createLog(_id, `Update Profile for ${username}`, req);
+            return await updateProfile(_id, body);
+        });
+    });
+    
+    rtr.post('/changePassword', (req, res)=>{
+        CtrlHandler(req, res, async(body)=>{
+            const {username}=res.locals.udata;
+            const {password, current}=body;
+            await changePassword(username, current, password);
+            createLog(_id, `Change password for ${username}`, req);
+            return password;
+        });
+    });
+
+    return rtr;
+}
